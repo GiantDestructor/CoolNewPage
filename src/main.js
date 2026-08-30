c6e97b184f41c568c49c70047eb9dbc14f88da4d
Giant <destructorbuck@gmail.com>
2026-08-29 21:16:18 -0600

    Added sounds and font
    Background music added as well as sound for interaction with keyboard and click on D-pad

--- src/main.js
+++ src/main.js
@@ -1,12 +1,38 @@
 //Declared variables
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
-const cross = document.getElementById("cross")
-const cross2 = document.getElementById("cross2")
-let time 
-let author
-let title
-let numOpciones = 0
-let opciones
+const cross = document.getElementById("cross");
+const cross2 = document.getElementById("cross2");
+
+//Import music
+import musicSrc from "./sounds/menu.mp3";
+import clicksound from "./sounds/pup.mp3"
+import click1 from "./sounds/click1.mp3";
+import click2 from "./sounds/click2.mp3";
+import click3 from "./sounds/click3.mp3";
+import click4 from "./sounds/click4.mp3";
+import click5 from "./sounds/click5.mp3";
+import click6 from "./sounds/click6.mp3";
+
+//array of the keyboard sounds
+const keySounds = [
+    new Audio(click1),
+    new Audio(click2),
+    new Audio(click3),
+    new Audio(click4),
+    new Audio(click5),
+    new Audio(click6)
+];
+
+let time;
+let author;
+let title;
+let numOpciones = 0;
+let opciones;
+
+const music = new Audio(musicSrc); 
+const click = new Audio(clicksound);
+
+
 //API Call to NASA to get info
 document.querySelector("#app").innerHTML = ""
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).then(response => response.json())
@@ -42,7 +68,8 @@ function showElements()
     opciones = [`<p id="time"></p>
             <form action="https://www.google.com/search" class="searchform" method="get" name="searchform" target="_blank">
             <input name="sitesearch" type="hidden" value="" placeholder="">
-            <input autocomplete="on" class="form-control search" name="q" placeholder="Search for Something!" required=""  type="text" style="border-radius: 15px; border: 1px solid black;"></form>`,
+            <input autocomplete="on" class="form-control search" name="q" placeholder="Search for Something!" required=""  type="text" style="border-radius: 15px; border: 1px solid black;"></form>
+            <p>Try clicking the D-pad!</p>`,
             `<h2>Author: </h2> <p>${author}</p>
              <h2>Title: </h2> <p>${title}</p>`,
   ] 
@@ -51,6 +78,11 @@ function showElements()
   //prints the info in the screen
   document.querySelector("#screen2").innerHTML = opciones[numOpciones];
   getTime();
+  music.loop = true;
+  music.volume = 0.2;
+  music.play();
+  
+
 }
 
 //gets time and updates it on the screen
@@ -72,7 +104,9 @@ setInterval(getTime, 1000);
 
 //Manages the options
 function funcOpcion()
-{
+{ 
+  
+  click.play()
   console.log(numOpciones)
   numOpciones++;
   if (numOpciones > opciones.length -1)
@@ -87,3 +121,19 @@ function funcOpcion()
 // what happens when you press D-pad
 cross.addEventListener("click", funcOpcion);
 cross2.addEventListener("click", funcOpcion);
+
+// added for the phone users
+cross.addEventListener("touchend", funcOpcion);
+cross2.addEventListener("touchend", funcOpcion);
+
+//plays the sound of the keyboard
+document.addEventListener("keydown", function(event) {
+  console.log(`Key pressed: ${event.key}`);
+  console.log(`Key code: ${event.code}`);
+  let keynumber
+  keynumber = Math.round(Math.random() * (6 - 1) + 1);
+  console.log(Math.round(keynumber));
+  keySounds[keynumber - 1].volume = 1;
+  keySounds[keynumber - 1].play();
+  
+});
